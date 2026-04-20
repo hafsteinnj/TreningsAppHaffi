5b8e03e46016c37419dc9028c5e668edd54106f2
hafst <hafst@TILVERAN>
2026-04-20 16:09:48 +0200

    Add project files.

---
+++ TreningsAppHaffi/Pages/Changelog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TreningsAppHaffi.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
