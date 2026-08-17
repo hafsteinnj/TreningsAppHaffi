7bf0f70d7cb948c99588449b4036b891ea31a9de
Hafsteinn Jonatann <hafsteinnj@gmail.com>
2026-08-17 23:23:15 +0200

    Fiks av klassenavn i Changelog.cshtml.cs,   opdatering av forsiden med ny plan for de neste 10-12 dagene, opdatering av changelog med tilsvarende informasjon.

--- TreningsAppHaffi/Pages/Changelog.cshtml.cs
+++ TreningsAppHaffi/Pages/Changelog.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TreningsAppHaffi.Pages
 {
-    public class Index1Model : PageModel
+    public class ChangelogModel : PageModel
     {
         public void OnGet()
         {
