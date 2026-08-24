2ed46fa2bc4c844220d6dd2c897b06a7311de2f9
Hafsteinn Jonatann <hafsteinnj@gmail.com>
2026-08-24 21:40:53 +0200

    Oppdatert til nyeste xUnits.v3 i stedenfor den 'mest nedlastede' versionen jeg startet med, oppdatert xUnit.runner.visualstudio til 4.0.0, lagt til Framework Core InMemory provider for en SQLtest, test.

--- TreningsAppHaffi.Tests/SQLtestModelTests.cs
+++ TreningsAppHaffi.Tests/SQLtestModelTests.cs
@@ -3,9 +3,25 @@
     public class SQLtestModelTests
     {
         [Fact]
-        public void Test1()
+        public async Task OnPostInsert_SetsHiddenToFalse()
         {
-         
+            /*
+             * Gjort som en lengre oppgave med detaljerte beskrivelser av ChatGPT.
+             * Første oppgaven gjaldt å ha 'arrange, act, assert' tydelig i tankene når man skriver tester.
+             * Og denne metoden ble spesifikt valgt for å demonstrere hvordan man 'takler' en 'fake' in-memory-database input i en test.
+             */
+            // Arrange
+            // Microsoft.EntityFrameworkCore.InMemory installert igjenom NuGet for å kunne bruke InMemoryDatabase for testing.
+            // <PackageReference Include="Microsoft.EntityFrameworkCore.InMemory" Version="10.0.3" />
+            // Fordi AzureSql serveren bruker den eldre 10.0.3 versionen og ikke 10.0.11 som er nyeste nytt. Og dette skal matche.
+            // Create SQLtestModel
+            // Give it some input
+
+            // Act
+            //await model.OnPostInsert();
+
+            // Assert
+            //entry.Hidden.Should().BeFalse();
         }
     }
 }
