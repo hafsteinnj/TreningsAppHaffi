d3cdb293f0296c3b9676ae22b2e07fc889d33501
Hafsteinn Jonatann <hafsteinnj@gmail.com>
2026-09-08 12:15:17 +0200

    glemte lage link til siden.

--- TreningsAppHaffi/Pages/NavApiList.cshtml.cs
+++ TreningsAppHaffi/Pages/NavApiList.cshtml.cs
@@ -1,12 +1,21 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TreningsAppHaffi.Services;
 
-namespace TreningsAppHaffi.Pages
+namespace TreningsAppHaffi.Pages;
+
+public class NavApiListModel : PageModel
 {
-    public class NavApiListModel : PageModel
+    private readonly NavApiClient _navApiClient;
+
+    public string? ApiResult { get; set; }
+
+    public NavApiListModel(NavApiClient navApiClient)
     {
-        public void OnGet()
-        {
-        }
+        _navApiClient = navApiClient;
+    }
+
+    public async Task OnGetAsync()
+    {
+        ApiResult = await _navApiClient.GetFeedAsync();
     }
 }
